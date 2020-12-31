870cf30a1c728bc1f5ef81927c856e0be9bd7273
Jeff Bailey <jbailey@raspberryginger.com>
2020-12-30 23:34:46 -0800

    Add test function and data.

---
+++ test/SnowballStemmer_test.dart
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import 'dart:collection';
+import 'dart:core';
+import 'dart:io';
+
+import '../lib/SnowballStemmer.dart';
+
+final testData = HashMap<String, String>();
+final testDataRegExp = RegExp(r'^([^\s]+)\s+([^\s]+)$');
+
+int passes = 0;
+int fails = 0;
+
+main() async {
+  await loadTestData();
+
+  var stemmer = SnowballStemmer();
+
+  for (var key in testData.keys) {
+    if (stemmer.stem(key) == testData[key]) {
+      passes++;
+    } else {
+      fails++;
+    }
+  }
+
+  print("passes: " + passes.toString());
+  print("fails: " + fails.toString());
+}
+
+Future<void> loadTestData() async {
+  File data = new File("snowball_test.txt");
+  await data.readAsLines().then(processLines);
+}
+
+void processLines(List<String> lines) {
+  for (var line in lines) {
+    if (line[0] == '#') continue;
+    Iterable<Match> words = testDataRegExp.allMatches(line);
+    for (var match in words) {
+      testData[match.group(1)] = match.group(2);
+    }
+  }
+}
